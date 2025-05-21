aea2f8e7007443e1feb69cfc644154e9d0b9e5fc
Seohyeon <0205lucy@naver.com>
2025-05-21 23:32:14 +0900

    fix: 아이콘 이미지 업데이트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,8 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/favicon.ico",
-        sizes: "any",
+        url: "/icon.png",
+        type: "image/png",
       },
     ],
   },
